9aab17631b66064a72342d93a47c2c59dba4d65d
Barish Barla <barish.barla10@gmail.com>
2022-06-23 20:22:37 +0530

    Added onscreen instructions
    Added a modal which shows instruction on how to draw an erase cell

--- src/app/ui.ts
+++ src/app/ui.ts
@@ -4,6 +4,9 @@ const gameStatus = document.querySelector(".status") as HTMLHeadingElement
 const startBtn = document.querySelector("#start") as HTMLButtonElement
 const pauseBtn = document.querySelector("#pause") as HTMLButtonElement
 const resetBtn = document.querySelector("#reset") as HTMLButtonElement
+const modalBtn = document.querySelector("#modal-btn") as HTMLButtonElement
+const modal = document.querySelector(".modal") as HTMLDivElement
+const card = document.querySelector(".card") as HTMLDivElement
 
 export const updateGameStatus = (s: string) => {
   gameStatus.innerText = s
@@ -19,4 +22,11 @@ pauseBtn.onclick = () => {
 
 resetBtn.onclick = () => {
   resetGame()
+}
+
+modalBtn.onclick = async () => {
+  await card.animate({
+    transform: ['scale(1)', 'scale(0.6)'],
+  }, 140).finished
+  modal.remove()
 }
